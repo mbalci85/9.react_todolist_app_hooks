ff08d0fe42ac98b4fe49882f3c1740877ea8fd97
Mustafa Balci <72220498+mbalci85@users.noreply.github.com>
2021-02-22 01:48:50 +0000

    added links

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 import { uuid } from 'uuidv4';
 import Todos from './components/Todos';
 import AddTodo from './components/AddTodo';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 import About from './pages/About';
 
 function App() {
@@ -55,34 +55,50 @@ function App() {
 				<h1 style={{ margin: 20, fontWeight: 'bolder', fontSize: 50 }}>
 					TODO LIST
 				</h1>
-
-				<Route
-					path="/"
+				<Link
+					to="/"
 					exact
-					render={() => (
-						<React.Fragment>
-							<AddTodo addToDo={addToDo} />
-							{todos.length !== 0 ? (
-								<Todos
-									todos={todos}
-									toggleCompleted={toggleCompleted}
-									deleteTodo={deleteTodo}
-								/>
-							) : (
-								<h2
-									style={{
-										color: 'coral',
-										marginTop: 100,
-										fontWeight: 'bolder',
-									}}
-								>
-									No Task to Show
-								</h2>
-							)}
-						</React.Fragment>
-					)}
-				/>
-				<Route path="/about" component={About} />
+					style={{ textDecoration: 'none', marginBottom: 10 }}
+				>
+					Home |{' '}
+				</Link>
+				<Link
+					to="/about"
+					style={{ textDecoration: 'none', marginBottom: 10 }}
+				>
+					About
+				</Link>
+				<br />
+				<br />
+				<Switch>
+					<Route
+						path="/"
+						exact
+						render={() => (
+							<React.Fragment>
+								<AddTodo addToDo={addToDo} />
+								{todos.length !== 0 ? (
+									<Todos
+										todos={todos}
+										toggleCompleted={toggleCompleted}
+										deleteTodo={deleteTodo}
+									/>
+								) : (
+									<h2
+										style={{
+											color: 'coral',
+											marginTop: 100,
+											fontWeight: 'bolder',
+										}}
+									>
+										No Task to Show
+									</h2>
+								)}
+							</React.Fragment>
+						)}
+					/>
+					<Route path="/about" component={About} />
+				</Switch>
 			</div>
 		</Router>
 	);
