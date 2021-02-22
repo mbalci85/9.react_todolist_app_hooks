18b5404fe4f912f937d1e53521db9f269280f88c
Mustafa Balci <72220498+mbalci85@users.noreply.github.com>
2021-02-22 01:03:42 +0000

    added style to todos

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -3,7 +3,17 @@ import React from 'react';
 const Todo = ({ todo }) => {
 	return (
 		<div>
-			<h2>{todo.task}</h2>
+			<h2
+				style={{
+					backgroundColor: '#f3f3f3',
+					textAlign: 'left',
+					padding: 20,
+					textDecoration: todo.completed ? 'line-through' : 'none',
+				}}
+			>
+				<input type="checkbox" style={{ marginRight: 10 }} />
+				{todo.task}
+			</h2>
 		</div>
 	);
 };
