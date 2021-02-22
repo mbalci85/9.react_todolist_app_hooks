147cd50770f318903a677152cf8b922785002548
Mustafa Balci <72220498+mbalci85@users.noreply.github.com>
2021-02-22 00:52:27 +0000

    added state with useState

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
 import './App.css';
+import React, { useState } from 'react';
+import { uuid } from 'uuidv4';
 
 function App() {
-	return <div className="App"></div>;
+	const [todos, setTodos] = useState([
+		{
+			id: uuid(),
+			task: 'Go',
+			completed: false,
+		},
+		{
+			id: uuid(),
+			task: 'Do',
+			completed: false,
+		},
+		{
+			id: uuid(),
+			task: 'Come',
+			completed: false,
+		},
+	]);
+	return (
+		<div className="App">
+			<h1>TODO LIST</h1>
+		</div>
+	);
 }
 
 export default App;
