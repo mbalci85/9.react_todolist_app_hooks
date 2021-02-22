ec471106bcedc40ef6ec41039ce34e25acba16fb
Mustafa Balci <72220498+mbalci85@users.noreply.github.com>
2021-02-22 00:58:24 +0000

    showed each todo

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
-const Todo = () => {
-	return <div></div>;
+const Todo = ({ todo }) => {
+	return (
+		<div>
+			<h2>{todo.task}</h2>
+		</div>
+	);
 };
 
 export default Todo;
